69fc8e4b039702c3e6679c6b50f26c0aa55c80df
Alex <juzzle@mail.ru>
2022-11-16 17:14:44 +0300

    changes code

--- C#/Program.cs
+++ C#/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Result homework
+
+
+string [] arrayStr = {"Hello","World","2",":-)"};
+string [] arrayStr2 = {"1234","1567","-2","Computer sience"};
+string [] arrayStr3 = {"Russia","Denmark","Kazan"};
+
+
+string[] createArray(string[] array,int count){
+        string[] resultArray = new string[count];
+
+
+    return resultArray;
+}
