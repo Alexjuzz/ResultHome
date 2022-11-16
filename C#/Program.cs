a09c4e962e42d4ad546af395d4b00356d5697995
Alex <juzzle@mail.ru>
2022-11-16 17:38:40 +0300

    complite

--- C#/Program.cs
+++ C#/Program.cs
@@ -1,14 +1,47 @@
 ﻿// Result homework
 
 
-string [] arrayStr = {"Hello","World","2",":-)"};
-string [] arrayStr2 = {"1234","1567","-2","Computer sience"};
-string [] arrayStr3 = {"Russia","Denmark","Kazan"};
+string[] arrayStr = { "Hello", "World", "2", ":-)" };
+string[] arrayStr2 = { "1234", "1567", "-2", "Computer sience" };
+string[] arrayStr3 = { "Russia", "Denmark", "Kazan" };
 
+string[] createArray(string[] array, int count)
+{
+    string[] resultArray = new string[count];
 
-string[] createArray(string[] array,int count){
-        string[] resultArray = new string[count];
-
-
+    int k = 0;
+    foreach (var item in array)
+    {
+        if (item.Length <= 3)
+        {
+            resultArray[k] = item;
+            k++;
+        }
+    }
     return resultArray;
 }
+
+int countValues(string[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(array[i].Length <=3){
+            count++;
+        }
+    }
+   
+    return count;
+}
+
+void printArr(string[] array)
+{
+    foreach (var item in array)
+    {
+        Console.WriteLine(item);
+    }
+
+}
+
+
+printArr(createArray(arrayStr3,countValues(arrayStr3)));
